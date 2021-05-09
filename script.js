152478b7cb980698440b504e65ccbfa9185c1623
Victor Hüni <victor.hueni@protonmail.com>
2021-05-09 23:28:29 +0200

    Adding picture in picture features

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('testing')
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Promt to select media stream, pass to video element the play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        console.log('Whoops an error in getting the video', error);
+    }
+}
+
+button.addEventListener('click', async() => {
+    //Disable the button
+    button.disabled = true;
+
+    //Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+
+    //Reset button
+    button.disabled = false;
+});
+
+
+
+//On Load
+selectMediaStream();
